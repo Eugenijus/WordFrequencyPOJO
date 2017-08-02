972ec67d7ea420ff596345595b5adfbd49538d29
Eugenijus Sabaliauskas <e.sabaliauskas@gmail.com>
2017-08-03 00:03:36 +0300

    init commit

---
+++ src/info/eugenijus/wordfrequency/WordFrequencyMain.java
@@ -0,0 +1,54 @@
+package info.eugenijus.wordfrequency;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Eug
+ *
+ */
+public class WordFrequencyMain {
+	
+	public Map<String, Integer> getWordFrequenciesFromCollection(List<String> strings_list){
+		
+		//Creating a separate list to iterate and update
+		List<String> list = new ArrayList<>(strings_list);
+		
+		//list.replaceAll(String::toUpperCase);
+		Comparator<String> c = Comparator.naturalOrder();
+		list.sort(c);
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		
+		for(String word : list) {
+			if(map.containsKey(word)) {
+				map.put(word, map.get(word)+1);
+			} else {
+				map.put(word, 1);
+			}
+		}
+		return map;
+	}
+	
+	public File readFile(String filename) {
+		
+		return null;
+	}
+	
+	public Map<String, Integer> getWordFrequencies(String wordFileName){
+		
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/info/eugenijus/wordfrequency/TestWordFrequencyMain.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package info.eugenijus.wordfrequency;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Eug
+ *
+ */
+public class TestWordFrequencyMain {
+	
+	private WordFrequencyMain wf;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		wf = new WordFrequencyMain();
+		//maybe create test files here
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		wf = null;
+		//delete test files here
+	}
+
+	/**
+	 * Test method for {@link info.eugenijus.wordfrequency.WordFrequencyMain#getWordFrequenciesFromCollection(java.util.Collection)}.
+	 */
+	@Test
+	public void getWordFrequenciesFromCollection() {
+		//This list is unmodifiable
+		List<String> strings_list = Arrays.asList("java", "java", "javascript", "ruby", "angularjs",
+				"python", "python", "assembler");
+						
+		Map<String, Integer> map = wf.getWordFrequenciesFromCollection(strings_list);
+		
+		System.out.println(map.toString());
+		
+		//map.forEach((key, value) -> System.out.println(key + " " + value));
+		
+		assertTrue(map.get("java").intValue() == 2);
+	}
+	
+	/**
+	 * Test method for {@link info.eugenijus.wordfrequency.WordFrequencyMain#readFile(java.lang.String)}.
+	 */
+	@Test
+	public void testReadFile() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link info.eugenijus.wordfrequency.WordFrequencyMain#getWordFrequencies(java.lang.String)}.
+	 */
+	@Test
+	public void testGetWordFrequencies() {
+		fail("Not yet implemented");
+	}
+
+}
